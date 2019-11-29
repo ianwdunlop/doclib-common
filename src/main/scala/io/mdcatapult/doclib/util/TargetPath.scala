fabd930e838a5218df268879c6a058987bf390c0
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2019-11-29 12:10:21 +0000

    Update target path method to include scrub. See #3

--- src/main/scala/io/mdcatapult/doclib/util/TargetPath.scala
+++ src/main/scala/io/mdcatapult/doclib/util/TargetPath.scala
@@ -1,14 +1,18 @@
 package io.mdcatapult.doclib.util
 
-import org.apache.commons.io.FilenameUtils
+import java.nio.file.Paths
+
+import com.typesafe.config.Config
 
 trait TargetPath {
 
+  val consumerPath: String
+  val doclibConfig: Config
   /**
-    * determines common root paths for two path string
-    * @param paths List[String]
-    * @return String common path component
-    */
+   * determines common root paths for two path string
+   * @param paths List[String]
+   * @return String common path component
+   */
   protected def commonPath(paths: List[String]): String = {
     val SEP = "/"
     val BOUNDARY_REGEX = s"(?=[$SEP])(?<=[^$SEP])|(?=[^$SEP])(?<=[$SEP])"
@@ -21,15 +25,33 @@ trait TargetPath {
   }
 
   /**
-    * generate new file path maintaining file path from origin but allowing for intersection of common root paths
-    * @param source String
-    * @return String full path to new target
-    */
-  def getTargetPath(source: String, base: String): String = {
+   * generate new file path maintaining file path from origin but allowing for intersection of common root paths
+   * @param source String
+   * @return String full path to new target
+   */
+  def getTargetPath(source: String, base: String, prefix: Option[String] = None): String = {
     val targetRoot = base.replaceAll("/+$", "")
-    val sourceName = FilenameUtils.removeExtension(source)
-    val c = commonPath(List(targetRoot, sourceName))
-    val scrubbed = sourceName.replaceAll(s"^$c", "").replaceAll("^/+|/+$", "")
-    s"$targetRoot/$scrubbed/"
+    val regex = """(.*)/(.*)$""".r
+    source match {
+      case regex(path, file) ⇒
+        val c = commonPath(List(targetRoot, path))
+        val targetPath  = scrub(path.replaceAll(s"^$c", "").replaceAll("^/+|/+$", ""))
+        Paths.get(doclibConfig.getString("doclib.local.temp-dir"), targetRoot, targetPath, s"${prefix.getOrElse("")}-$file").toString
+      case _ ⇒ source
+    }
+  }
+
+  /**
+   * Replace repeated doclib path entries with single.
+   * eg "/derivatives/derivatives" with "/derivatives"
+   * @param path
+   * @return
+   */
+  protected def scrub(path: String):String  = path match {
+    case path if path.startsWith(doclibConfig.getString("doclib.local.target-dir")) ⇒
+      scrub(path.replaceFirst(s"^${doclibConfig.getString("doclib.local.target-dir")}/*", ""))
+    case path if path.startsWith(doclibConfig.getString(consumerPath))  ⇒
+      scrub(path.replaceFirst(s"^${doclibConfig.getString(consumerPath)}/*", ""))
+    case _ ⇒ path
   }
 }
