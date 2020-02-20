f2bb0f77e2a985b6f0ba30da5a031a39470c2e51
Daniel Sheppard <daniel.sheppard@md.catapult.org.uk>
2020-02-20 16:06:45 +0000

    UnlimitedLimitedExecution is now an object and extends LimitedExecutionFactory that it can be treated like SemaphoreLimitedExecution.

--- src/main/scala/io/mdcatapult/doclib/concurrency/UnlimitedLimitedExecution.scala
+++ src/main/scala/io/mdcatapult/doclib/concurrency/UnlimitedLimitedExecution.scala
@@ -4,7 +4,9 @@ import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent.{ExecutionContext, Future}
 
 /** A stub implementation of LimitedExecution that makes no attempt to actually limit execution. */
-class UnlimitedLimitedExecution extends LimitedExecution with LazyLogging {
+object UnlimitedLimitedExecution extends LimitedExecution with LazyLogging with LimitedExecutionFactory {
+
+  override def create(concurrency: Int): LimitedExecution = this
 
   override def weighted[C, T](weight: Int)(c: C, label: String)(f: C => Future[T])(implicit ec: ExecutionContext): Future[T] =
     unlimited(c, label)(f)
