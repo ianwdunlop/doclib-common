01ea66e8f3c795feef64fa07070176c1d08a4c05
Daniel Sheppard <daniel.sheppard@md.catapult.org.uk>
2020-04-20 14:07:06 +0100

    text fragment id is UUID.

--- src/main/scala/io/mdcatapult/doclib/models/document/TextFragment.scala
+++ src/main/scala/io/mdcatapult/doclib/models/document/TextFragment.scala
@@ -1,12 +1,14 @@
 package io.mdcatapult.doclib.models.document
 
+import java.util.UUID
+
 import org.mongodb.scala.bson.ObjectId
 
 case class TextFragment(
-                       _id: ObjectId,
-                       document: ObjectId,
-                       index: Int,
-                       startAt: Int,
-                       endAt: Int,
-                       length: Int
+                         _id: UUID,
+                         document: ObjectId,
+                         index: Int,
+                         startAt: Int,
+                         endAt: Int,
+                         length: Int
                        )
