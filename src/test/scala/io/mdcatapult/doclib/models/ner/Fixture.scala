1a709d5d93d20f76381dc3692ae1417d1d945e54
Daniel Sheppard <daniel.sheppard@md.catapult.org.uk>
2020-04-21 13:41:02 +0100

    Remove unused code.

--- src/test/scala/io/mdcatapult/doclib/models/ner/Fixture.scala
+++ src/test/scala/io/mdcatapult/doclib/models/ner/Fixture.scala
@@ -1,14 +1,14 @@
 package io.mdcatapult.doclib.models.ner
 
 import java.util.UUID
+import java.util.UUID.fromString
 
 object Fixture {
 
-  val uuid: UUID = UUID.fromString("dc83cac6-4daa-4a0b-8e52-df1543af1e8f")
-  val docUuid: UUID = UUID.fromString("600029ba-ccea-4e46-9ea5-7f54996954dd")
-  val fragmentUuid: UUID = UUID.fromString("f19e299d-d07e-434c-9cb7-9370832a7808")
+  val uuid: UUID = fromString("dc83cac6-4daa-4a0b-8e52-df1543af1e8f")
+  val docUuid: UUID = fromString("600029ba-ccea-4e46-9ea5-7f54996954dd")
+  val fragmentUuid: UUID = fromString("f19e299d-d07e-434c-9cb7-9370832a7808")
 
   val uuidMongoBinary = """{"$binary": "3IPKxk2qSguOUt8VQ68ejw==", "$type": "04"}"""
   val docUuidMongoBinary = """{"$binary": "YAApuszqTkaepX9UmWlU3Q==", "$type": "04"}"""
-  val fragmentUuidMongoBinary = """{"$binary": "YAApuszqTkaepX9UmWlU3Q==", "$type": "04"}"""
 }
