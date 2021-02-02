4157e4c721152a2a938f02fc735092d8922f79fa
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2021-02-02 14:18:57 +0000

    Document that handle method parameter key is the rabbit exchange

--- src/main/scala/io/mdcatapult/doclib/consumer/ConsumerHandler.scala
+++ src/main/scala/io/mdcatapult/doclib/consumer/ConsumerHandler.scala
@@ -8,9 +8,12 @@ import scala.concurrent.Future
 trait ConsumerHandler[T <: Envelope] {
 
   /**
-   * Process a message and return a DoclibDoc.
+   * Process a message and return a DoclibDoc. The handle method is passed into a
+   * Queue when subscribing to it and acts as a callback for any messages that the
+   * queue receives. The key (ie exchange) is extracted from the message within
+   * the Queue subscribe method.
    * @param message One of the various Envelopes eg PrefetchMsg, SupervisorMsg
-   * @param key
+   * @param key AMQP Exchange
    * @return DoclibDoc
    */
   def handle(message: T, key: String): Future[Option[DoclibDoc]]
